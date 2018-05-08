9a8f06e13651c0a0cf927f5c3c40a3e632b8d58e
Jorge Mendes <mendesjorge49@gmail,com>
2018-05-08 12:02:11 +0100

    configuração das ferramentas de autenticação -eliminar a referência ao nemaspace 'identitySample' -migrar a db 'MultasDB' para a bd de autenticação

--- multas/multas/Migrations/Configuration.cs
+++ multas/multas/Migrations/Configuration.cs
@@ -7,14 +7,14 @@ namespace multas.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<multas.Models.MultasDB>
+    internal sealed class Configuration : DbMigrationsConfiguration<multas.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(multas.Models.MultasDB context)
+        protected override void Seed(multas.Models.ApplicationDbContext context)
         {
             //*********************************************************************
             // adiciona AGENTES
---
+++ multas/multas/Models/IdentityModels.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace multas.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+        /// <summary>
+        /// class para efetuar a geração de utilizadores.
+        /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    /// <summary>
+    /// class responsavel pela criaçãoda base de dados da atenticação
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("MultasDbConnectionSring", throwIfV1Schema: false)
+        {
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        //vamos coloca, aqui, as intruções relativas às tabelas do 'negócio'
+
+        //descreve os nomes das tabelas na Base de dados
+        public virtual DbSet<Multas> Multas { get; set; }
+        public virtual DbSet<Viaturas> Viaturas { get; set; }
+        public virtual DbSet<Condutores> Condutores { get; set; }
+        public virtual DbSet<Agentes> Agentes { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
